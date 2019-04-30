6e8d5dfd822c94404c977f1e8e8575673a6441dc
diananapr <43902810+diananapr@users.noreply.github.com>
2019-04-30 12:28:28 +0700

    home
    buat navbarnya (home, camera. tips), isi halaman home: bmi doang.

--- app/src/main/java/com/snapdiet/MainActivity.java
+++ app/src/main/java/com/snapdiet/MainActivity.java
@@ -1,7 +1,15 @@
 package com.snapdiet;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = (BottomNavigationView) findViewById(R.id.bottomnav);
+        BottomNavigationView bottomNav1 = (BottomNavigationView) findViewById(R.id.bottomnav1);
+        BottomNavigationViewHelper.disableShiftMode(bottomNav);
+
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        bottomNav1.setOnNavigationItemSelectedListener(navListener);
+
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+        new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                switch (item.getItemId()){
+                    case R.id.navigation_home:
+                        selectedFragment = new HomeFragment();
+                        break;
+                    case R.id.navigation_camera:
+                        selectedFragment = new TipsFragment();
+                        break;
+                    case R.id.navigation_tips:
+                        selectedFragment = new TipsFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                return true;
+            }
+        };
+
+
 }
--- app/src/main/java/com/snapdiet/WelcomeActivity.java
+++ app/src/main/java/com/snapdiet/WelcomeActivity.java
@@ -1,6 +1,7 @@
 package com.snapdiet;
 
 import android.content.Intent;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -78,8 +79,10 @@ public class WelcomeActivity extends AppCompatActivity {
         btnGetStarted.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Intent intent = new Intent(WelcomeActivity.this,MainActivity.class);
                 startActivity(intent);
+
             }
         });
     }
