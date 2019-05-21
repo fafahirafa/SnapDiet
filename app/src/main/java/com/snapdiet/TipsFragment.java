d064d114862ddaf0d1a5e5c8bdcd2dab51cc64d4
fafahirafa <39693392+fafahirafa@users.noreply.github.com>
2019-05-21 11:44:52 +0700

    first commit

--- app/src/main/java/com/snapdiet/TipsFragment.java
+++ app/src/main/java/com/snapdiet/TipsFragment.java
@@ -1,19 +1,102 @@
 package com.snapdiet;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.gson.Gson;
+import com.snapdiet.Adapter.FeedAdapter;
+import com.snapdiet.Common.HTTPDataHandler;
+import com.snapdiet.Model.RSSObject;
+
 public class TipsFragment extends Fragment {
 
+    Toolbar toolbar;
+    RecyclerView recyclerView;
+    RSSObject rssObject;
+
+    //RSS Link
+    private final String RSS_link="https://www.medicinenet.com/rss/general/diet_and_weight_management.xml";
+    private final String RSS_to_Json_API="https://api.rss2json.com/v1/api.json?rss_url=";
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+
         return inflater.inflate(R.layout.fragment_tips, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        toolbar =(Toolbar) getView().findViewById(R.id.toolbar);
+        toolbar.setTitle("News");
+        recyclerView = getView().findViewById(R.id.recyclerView);
+
+//        LinearLayoutManager linearLayoutManager= new LinearLayoutManager( getActivity().getBaseContext(),LinearLayoutManager.VERTICAL,false);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        loadRSS();
+    }
+
+    private void loadRSS() {
+        AsyncTask<String,String,String> loadRSSAsync = new AsyncTask<String, String, String>() {
+
+            ProgressDialog mDialog = new ProgressDialog(getActivity());
+
+
+            @Override
+            protected void onPreExecute() {
+                mDialog.setMessage("Please Wait...");
+                mDialog.show();
+            }
+
+
+            @Override
+            protected String doInBackground(String... params) {
+                String result;
+                HTTPDataHandler http = new HTTPDataHandler();
+                result = http.GetHTTPData(params[0]);
+                return result;
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                mDialog.dismiss();
+                rssObject=new Gson().fromJson(s,RSSObject.class);
+                FeedAdapter adapter = new FeedAdapter(rssObject,getActivity().getBaseContext());
+                recyclerView.setAdapter(adapter);
+                adapter.notifyDataSetChanged();
+            }
+        };
+
+        StringBuilder url_get_data = new StringBuilder(RSS_to_Json_API);
+        url_get_data .append(RSS_link);
+        loadRSSAsync.execute(url_get_data.toString());
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()== R.id.menu_refresh)
+            loadRSS();
+        return true;
+    }
+
 }
