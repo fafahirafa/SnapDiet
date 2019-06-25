97212a717f3359a9fbef7a9d99031daf57ae4b05
diananapr <43902810+diananapr@users.noreply.github.com>
2019-06-25 13:52:29 +0700

    ganti font
    ganti font di home, calorie finder, slider

--- app/src/main/java/com/snapdiet/HomeFragment.java
+++ app/src/main/java/com/snapdiet/HomeFragment.java
@@ -13,6 +13,7 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -152,7 +153,9 @@ public class HomeFragment extends Fragment {
                         tvListMakanan.setText("no foods");
                 } else {
                     tvListMakanan.setText("no data exist");
+                    //tvTotalKalori.setGravity(Gravity.RIGHT);
                     tvTotalKalori.setText("no data exist");
+
                 }
             }
 
